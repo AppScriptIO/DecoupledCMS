32a862577057deab75475e8e641be33b95ebae4d
Dr. Safi <sfmissive@gmail.com>
2017-12-13 13:19:18 +0200

    Update execution implementation for shellscript unit - working.

--- module/reusableNestedUnit/implementation/shellscript/Unit.class.js
+++ module/reusableNestedUnit/implementation/shellscript/Unit.class.js
@@ -2,6 +2,7 @@ import { classDecorator as prototypeChainDebug} from 'appscript/module/prototype
 import { add, execute, applyMixin, conditional } from 'appscript/utilityFunction/decoratorUtility.js'
 import { extendedSubclassPattern } from 'appscript/utilityFunction/extendedSubclassPattern.js'
 import { curried as getTableDocumentCurried } from "appscript/utilityFunction/database/query/getTableDocument.query.js";
+import { exec, spawn, spawnSync } from 'child_process'
 
 let getDocument = {
     'Unit': getTableDocumentCurried({ documentId: 'shellscript_unit' })
@@ -17,7 +18,21 @@ export default ({ Superclass }) => {
         @extendedSubclassPattern.Subclass()
         class Unit extends Superclass {
             async executeScript() {
-                console.log(this)
+                switch (this.implementation) {
+                    case 'spawn':
+                        let message = ` _____                          _        
+| ____|__  __ ___   ___  _   _ | |_  ___ 
+|  _|  \\ \\/ // _ \\ / __|| | | || __|/ _ \\
+| |___  >  <|  __/| (__ | |_| || |_|  __/    
+|_____|/_/\\_\\\\___| \\___| \\__,_| \\__|\\___|
+${this.command} ${this.argument}`;
+                        console.log(message)
+                        spawnSync(this.command, this.argument, this.option)
+                    break;
+                    default:
+                        console.log('shellscriptUnit.implementation does not match any option.')
+                    break;
+                }
             }
         }
     return self
