d256a8e863e334ac07f3b900693d3d2a7c8914f4
Dr. Safi <sfmissive@gmail.com>
2017-12-15 09:21:45 +0200

    Fixed execution of shellscript using nodejs (in shellscript nestedunit).

--- module/reusableNestedUnit/implementation/shellscript/Unit.class.js
+++ module/reusableNestedUnit/implementation/shellscript/Unit.class.js
@@ -22,13 +22,12 @@ export default ({ Superclass }) => {
 | ____|__  __ ___   ___  _   _ | |_  ___ 
 |  _|  \\ \\/ // _ \\ / __|| | | || __|/ _ \\
 | |___  >  <|  __/| (__ | |_| || |_|  __/    
-|_____|/_/\\_\\\\___| \\___| \\__,_| \\__|\\___|
-${this.command} ${this.argument}`;
+|_____|/_/\\_\\\\___| \\___| \\__,_| \\__|\\___|`;
                 let childProcess;
                 switch (this.implementation) {
                     case 'spawn':
                         try {
-                            console.log(message)
+                            console.log(message); console.log(`${this.command} ${this.argument}`)
                             childProcess = spawnSync(this.command, this.argument, this.option)
                             if(childProcess.status > 0) throw childProcess.error
                         } catch (error) {
@@ -37,7 +36,7 @@ ${this.command} ${this.argument}`;
                     break;
                     case 'spawnAsynchronous':
                         try {
-                            console.log(message)
+                            console.log(message); console.log(`${this.command} ${this.argument}`)                            
                             childProcess = spawn(this.command, this.argument, this.option)
                             if(childProcess.status > 0) throw childProcess.error
                         } catch (error) {
@@ -46,19 +45,22 @@ ${this.command} ${this.argument}`;
                     break;
                     case 'file':
                         try {
-                            console.log(message)
+                            console.log(message); console.log(`shellscript file: ${this.filename}`)                            
                             let appBasePath = '/project/application/source/containerInstallationNodejs/shellScript/'
                             this.option.cwd = appBasePath
-                            childProcess = spawn('sh', this.filename,  this.option)
+                            childProcess = spawnSync(`sh`, `${this.filename}`, this.option)
                             if(childProcess.status > 0) throw childProcess.error
                         } catch (error) {
-                            process.exit(childProcess.status)
+                            let status = (childProcess) ? childProcess.status : 1;
+                            process.exit(status)
                         }
                     break;
                     default:
                         console.log('shellscriptUnit.implementation does not match any option.')
                     break;
                 }
+                // important to prevent 'unable to re-open stdin' error between shells.
+                await new Promise(resolve => setTimeout(resolve, 1000)) // wait x seconds before next script execution.
             }
         }
     return self
