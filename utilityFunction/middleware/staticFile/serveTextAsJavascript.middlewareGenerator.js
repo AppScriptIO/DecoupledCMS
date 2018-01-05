3a171e8c13d82bd648ae3cd8073b3fe874e4f67b
Dr. Safi <sfmissive@gmail.com>
2018-01-05 17:08:09 +0200

    Added template parsing through '$' sign in url for rendering serverside.

--- utilityFunction/middleware/staticFile/serveTextAsJavascript.middlewareGenerator.js
+++ utilityFunction/middleware/staticFile/serveTextAsJavascript.middlewareGenerator.js
@@ -7,17 +7,30 @@ import send from 'koa-sendfile' // Static files.
 import filesystem from 'fs'
 import Stream from 'stream'
 import multistream from 'multistream'
+import underscore from 'underscore'
+import Application from 'appscript'
 
 // read streams and send them using koa - https://github.com/koajs/koa/issues/944 http://book.mixu.net/node/ch9.html
 // returns a middleware object 
 export default function serveStaticDirectory(setting) {
     let middleware = async (context, next) => {
         let fileRelativePath = context.path.substr(0, context.path.lastIndexOf('$')) // remove function name
-        let filePath = path.resolve(path.normalize(`${context.instance.config.clientBasePath}${fileRelativePath}`)) 
+        let filePath = path.join(context.instance.config.clientBasePath, fileRelativePath) 
+
+        let fileDirectoryPath = filePath.substr(0, filePath.lastIndexOf('/'))
+        let argument = { layoutElement: 'webapp-layout-list' }
+        let view = {};
+
+        let templatePart = {
+            css: underscore.template(filesystem.readFileSync(`${fileDirectoryPath}/.css`, 'utf8'))({ Application, argument}),
+            js: underscore.template(filesystem.readFileSync(`${fileDirectoryPath}/.js`, 'utf8'))({ Application, argument}),
+            html: underscore.template(filesystem.readFileSync(`${fileDirectoryPath}/.html`, 'utf8'))({ Application, argument}),
+        }
         try {
-            let stream = await covertTextFileToJSModule({ filePath })
-            context.body = stream // Koa handles the stream and send it to the client.
-            context.type = 'application/javascript'
+            let content = filesystem.readFileSync(filePath, 'utf8')
+            let rendered = underscore.template(content)({ Application, view, argument: Object.assign(argument, templatePart)})
+            context.body = rendered // Koa handles the stream and send it to the client.
+            // context.type = 'application/javascript'
         } catch (error) {
          console.log(error)
          await next()
@@ -38,3 +51,4 @@ async function covertTextFileToJSModule({ filePath }) {
     afterStream.push(null)
     return multistream([beforeStream, fileStream, afterStream])
 }
+
