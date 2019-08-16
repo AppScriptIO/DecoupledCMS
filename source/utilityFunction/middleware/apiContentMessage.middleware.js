23fc993dd91299a54203a7455f1ad45f81a67b67
Safi <sfmissive@gmail.com>
2019-08-16 05:45:51 +0300

    👕 Lint JS code.

--- source/utilityFunction/middleware/apiContentMessage.middleware.js
+++ source/utilityFunction/middleware/apiContentMessage.middleware.js
@@ -2,6 +2,6 @@ import { default as Application } from '../../class/Application.class.js'
 import createStaticInstanceClasses from '@dependency/graphTraversal'
 
 export default async (context, next) => {
-    let connection = Application.rethinkdbConnection
-    context.body = 'Provide an entrypoint for the api schema.'
+  let connection = Application.rethinkdbConnection
+  context.body = 'Provide an entrypoint for the api schema.'
 }
